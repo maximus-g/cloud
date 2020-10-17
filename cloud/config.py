af0cdb258f7fa7e8d4f209b53ab0c7bf8a2f24b7
binary <xolanigumbi19@gmail.com>
2020-10-17 18:15:44 +0200

    Modified config.py

--- cloud/config.py
+++ cloud/config.py
@@ -9,7 +9,7 @@ class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir, 'database.db')
-    FLASK_ENV = development
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     AUD_ALLOWED_EXTENSIONS = {'mp3', 'm4a', 'flac', 'wav', 'wma'}
     DOC_ALLOWED_EXTENSIONS = {'doc', 'docx', 'odt', 'pdf', 'xls', 'xlsx',\
         'ods', 'ppt', 'pptx', 'txt'}
